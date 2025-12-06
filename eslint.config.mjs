3de9a9436af438ceb43f89d7384abc2989e10353
Ali Khaled <ali.abdelqawy@outlook.com>
2025-12-06 05:41:04 +0200

    improve prettier configs (#16)
    * remove eslint prettier plugin
    
    * support all files not src files in format command
    
    * install @trivago/prettier-plugin-sort-imports
    
    * add prettier imports sort plugin configs
    
    * format files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import eslint from "@eslint/js";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig(
   { ignores: ["eslint.config.mjs"] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: { ...globals.node, ...globals.jest },
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/app.module";
 import { checkEngines } from "@/utils/check-engines.util";
+import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
   checkEngines();
